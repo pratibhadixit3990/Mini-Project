66c2de541d92ab8e5c8bee70287c5ae780553e16
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-10-17 11:36:36 +0530

    dash.php modified

--- dash.php
+++ dash.php
@@ -5,7 +5,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title> Admin || Skill's Breaker</title>
+<title> Admin || Online Quiz Generator</title>
 <link  rel="stylesheet" href="css/bootstrap.min.css"/>
  <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
  <link rel="stylesheet" href="css/main.css">
@@ -37,7 +37,7 @@ $(function () {
 <div class="header">
 <div class="row">
 <div class="col-lg-6">
-<span class="logo">Skill's Breaker</span></div>
+<span class="logo">Online Quiz Generator</span></div>
 <?php
 include_once 'dbConnection.php';
 session_start();
